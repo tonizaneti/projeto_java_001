f8dc3cc9e4d520a0f77ebf34c6a4b97c7ee03729
Antonio Delvair Zaneti <antonio.zaneti@gmail.com>
2018-06-26 17:27:10 -0300

    Add files via upload

---
+++ src/projeto01/Projeto01.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projeto01;
+
+/**
+ *
+ * @author Toni
+ */
+public class Projeto01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Ola Você!");
+        int i, j=50, res;
+        for (i=0; i<j; i++)
+        {
+            res=i*j;
+            System.out.println("Resultado = " +i +"*"+ j +"="+ res);
+        }
+    }
+    public class projeto01{
+        
+    }
+    }
+    
